ea52e8e620cc1cd85119161ebf8315dc1a5aebd2
larryquira <larry1595@hotmail.com>
2021-09-30 22:13:03 -0500

    Añadir titulo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+       <Switch>
+         <Route path ="/"> 
+
+
+         </Route>
+
+
+       </Switch>
+
+
+      </Router>
+      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
