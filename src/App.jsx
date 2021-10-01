4a8d9494c82c5ec7580d728255e1ee22b4c4e0a9
larryquira <larry1595@hotmail.com>
2021-10-01 01:31:31 -0500

    Anadir paginas inicio y registroproductos a react

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,33 @@
 import logo from './logo.svg';
-import './App.css';
+import Inicio from './pages/Inicio';
+import RegistroProductos from './pages/RegistroProductos';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './styles/Inicio.css';
 
 function App() {
   return (
     <div className="App">
       <Router>
        <Switch>
-         <Route path ="/"> 
 
-
+       <Route path ='/RegistroProductos'> 
+           <RegistroProductos/>
+         </Route>
+         
+         <Route path ='/'> 
+           <Inicio/>
          </Route>
 
+         
+
+         
 
        </Switch>
-
-
+       
       </Router>
       
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    </div>  
+      
   );
 }
 
