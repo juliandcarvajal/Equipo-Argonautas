909a0617964ddf52de1ddf58747dce859c34f290
juliandcarvajal <juliandcarvajal@gmail.com>
2021-10-02 21:27:32 -0500

    Ajuste a tipos de variables de Ingreso de productos

--- src/pages/gestorVentas.jsx
+++ src/pages/gestorVentas.jsx
@@ -13,15 +13,25 @@ import {
 } from "reactstrap";
 
 const data = [
-  { ID_Producto: 1, des_Producto: "Naruto", Cantidad: 1, Precio_Unitario:2.5},
-  { ID_Producto: 2, des_Producto: "Goku", Cantidad: 1, Precio_Unitario:1.4},
-  { ID_Producto: 3, des_Producto: "Kenshin Himura", Cantidad: 1,Precio_Unitario:2.5 },
-  { ID_Producto: 4, des_Producto: "Monkey D. Luffy", Cantidad: 1,Precio_Unitario:2.4 },
-  { ID_Producto: 5, des_Producto: "Edward Elric", Cantidad: 1,Precio_Unitario:2.3},
-  { ID_Producto: 6, des_Producto: "Seto Kaiba", Cantidad: 1 ,Precio_Unitario:1.2},
-  { ID_Producto: 7, des_Producto: "Seto Kaiba", Cantidad: 1 ,Precio_Unitario:1.7},
+  { ID_Producto: 1, des_Producto: "Naruto", Cantidad: 1, Precio_Unitario:2.5, Precio_Total:150},
+  { ID_Producto: 2, des_Producto: "Goku", Cantidad: 1, Precio_Unitario:1.4, Precio_Total:150},
+  { ID_Producto: 3, des_Producto: "Kenshin Himura", Cantidad: 1,Precio_Unitario:2.5, Precio_Total:150 },
+  { ID_Producto: 4, des_Producto: "Monkey D. Luffy", Cantidad: 1,Precio_Unitario:2.4, Precio_Total:150 },
+  { ID_Producto: 5, des_Producto: "Edward Elric", Cantidad: 1,Precio_Unitario:2.3, Precio_Total:150},
+  { ID_Producto: 6, des_Producto: "Seto Kaiba", Cantidad: 1 ,Precio_Unitario:1.2, Precio_Total:150},
+  { ID_Producto: 7, des_Producto: "Seto Kaiba", Cantidad: 1 ,Precio_Unitario:1.7, Precio_Total:150},
 ];
 
+const data2 = [
+    { ID_Producto: 1, des_Producto: "Naruto", Cantidad: 1, Precio_Unitario:2.5},
+    { ID_Producto: 2, des_Producto: "Goku", Cantidad: 1, Precio_Unitario:1.4},
+    { ID_Producto: 3, des_Producto: "Kenshin Himura", Cantidad: 1,Precio_Unitario:2.5 },
+    { ID_Producto: 4, des_Producto: "Monkey D. Luffy", Cantidad: 1,Precio_Unitario:2.4 },
+    { ID_Producto: 5, des_Producto: "Edward Elric", Cantidad: 1,Precio_Unitario:2.3},
+    { ID_Producto: 6, des_Producto: "Seto Kaiba", Cantidad: 1 ,Precio_Unitario:1.2},
+    { ID_Producto: 7, des_Producto: "Seto Kaiba", Cantidad: 1 ,Precio_Unitario:1.7},
+  ];
+  
 
 
 class GestorVentas extends React.Component {
@@ -66,6 +76,10 @@ class GestorVentas extends React.Component {
     this.setState({ modalInsertar: false });
   };
 
+  cerrarModalInsertar2 = () => {
+    this.setState({ modalInsertar2: false });
+  };
+
   editar = (dato) => {
     var opcion = window.confirm("Está seguro que desea actualizar el Resgistro de venta número  "+dato.ID_Producto);
     if(opcion ==true){
@@ -100,6 +114,14 @@ class GestorVentas extends React.Component {
   };
 
   insertar= ()=>{
+    var valorNuevo= {...this.state.form};
+    
+    var lista= this.state.data;
+    lista.push(valorNuevo);
+    this.setState({ modalInsertar: false, data: lista });
+  }
+
+  insertar2= ()=>{
     var valorNuevo= {...this.state.form};
     
     var lista= this.state.data;
@@ -203,6 +225,7 @@ class GestorVentas extends React.Component {
                   <td>{dato.des_Producto}</td>
                   <td>{dato.Cantidad}</td>
                   <td>{dato.Precio_Unitario}</td>
+                  <td>{dato.Precio_Total}</td>
                   <td>
                     <Button
                       color="primary"
@@ -234,7 +257,7 @@ class GestorVentas extends React.Component {
           <ModalBody>
             <FormGroup>
               <label>
-               ID_Producto:
+               ID Producto:
               </label>
             
               <input
@@ -249,7 +272,7 @@ class GestorVentas extends React.Component {
             
             <FormGroup>
               <label>
-                des_Producto: 
+                Descripción del Producto: 
               </label>
               <input
                 className="form-control"
@@ -266,16 +289,16 @@ class GestorVentas extends React.Component {
               </label>
               <input
                 className="form-control"
-                name="Valor_unitario"
+                name="Cantidad"
                 type="text"
                 onChange={this.handleChange}
-                value={this.state.form.Valor_unitario}
+                value={this.state.form.Cantidad}
               />
             </FormGroup>
 
             <FormGroup>
               <label>
-                Precio_Unitario: 
+                Precio Unitario: 
               </label>
               <input
                 className="form-control"
@@ -283,6 +306,19 @@ class GestorVentas extends React.Component {
                 type="text"
                 onChange={this.handleChange}
                 value={this.state.form.Precio_Unitario}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label>
+                Precio Total: 
+              </label>
+              <input
+                className="form-control"
+                name="Precio_Total"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.Precio_Total}
               />
             </FormGroup>
             
@@ -318,20 +354,20 @@ class GestorVentas extends React.Component {
           <ModalBody>
             <FormGroup>
               <label>
-                ID_Producto: 
+                ID Producto: 
               </label>
               
               <input
                 className="form-control"
                 name="ID_Producto"
-                type="text"
+                type="number"
                 onChange={this.handleChange}
               />
             </FormGroup>
             
             <FormGroup>
               <label>
-                des_Producto: 
+                Descripción del Producto: 
               </label>
               <input
                 className="form-control"
@@ -340,27 +376,40 @@ class GestorVentas extends React.Component {
                 onChange={this.handleChange}
               />
             </FormGroup>
-            
+
             <FormGroup>
               <label>
-                Valor_unitario: 
+                Cantidad: 
               </label>
               <input
                 className="form-control"
-                name="Valor_unitario"
-                type="text"
+                name="Cantidad"
+                type="number"
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+
+
+            <FormGroup>
+              <label>
+                Valor unitario: 
+              </label>
+              <input
+                className="form-control"
+                name="Precio_Unitario"
+                type="number"
                 onChange={this.handleChange}
               />
             </FormGroup>
 
             <FormGroup>
               <label>
-                Precio_Unitario: 
+                Precio Total: 
               </label>
               <input
                 className="form-control"
-                name="Precio_Unitario"
-                type="text"
+                name="Precio_Total"
+                type="number"
                 onChange={this.handleChange}
               />
             </FormGroup>
@@ -470,15 +519,15 @@ class GestorVentas extends React.Component {
           <ModalFooter>
             <Button
               color="primary"
-              onClick={() => this.insertar()}
+              onClick={() => this.insertar2()}
             >
-              Insertar
+              Guardar
             </Button>
             <Button
               className="btn btn-danger"
-              onClick={() => this.cerrarModalInsertar()}
+              onClick={() => this.cerrarModalInsertar2()}
             >
-              Cancelar
+              Cerrar
             </Button>
           </ModalFooter>
         </Modal>
