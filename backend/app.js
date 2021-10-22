76bceeb1beda51c44a73bcf5c4c8fe67347193b1
al_prieto <alejandroprieto.has@gmail.com>
2021-10-21 20:24:13 -0500

    Update Backend Products

--- backend/app.js
+++ backend/app.js
@@ -14,8 +14,17 @@ mongoose.connect("mongodb+srv://al_prieto:1234@cluster0.k1w6p.mongodb.net/myFirs
 });
 
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+    console.log(req)
+    next();
+});
 app.use("/api/products", productsRoutes)
 app.use("/api/ventas", ventasRoutes)
 app.use("/api/usuarios", usuariosRoutes)
+
 
 module.exports = app;
--- src/pages/Gestorproductos.jsx
+++ src/pages/Gestorproductos.jsx
@@ -14,14 +14,14 @@ import {
 } from "reactstrap";
 
 const data = [
-  { ID: 1, descripción: "Collar de perro", Valor_unitario: "$ 10.000", Estado:"Disponible"},
-  { ID: 2, descripción: "Gimnasio de gato pequeño", Valor_unitario: "$80.000", Estado:"Disponible"},
-  { ID: 3, descripción: "Comedero de perros", Valor_unitario: "$30.000",Estado:"Dispoible" },
-  { ID: 4, descripción: "Gimnasio  de gato grande", Valor_unitario: "$130.000",Estado:"No disponible" },
-  { ID: 5, descripción: "Galletas artesanales de perro ", Valor_unitario: "$6.000",Estado:"Disponible"},
-  { ID: 6, descripción: "Cama de perro", Valor_unitario: "$35.000" ,Estado:"Disponible"},
+  { ID: 1, descripción: "Collar de perro", Valor_unitario: "$ 10.000", Estado: "Disponible" },
+  { ID: 2, descripción: "Gimnasio de gato pequeño", Valor_unitario: "$80.000", Estado: "Disponible" },
+  { ID: 3, descripción: "Comedero de perros", Valor_unitario: "$30.000", Estado: "Dispoible" },
+  { ID: 4, descripción: "Gimnasio  de gato grande", Valor_unitario: "$130.000", Estado: "No disponible" },
+  { ID: 5, descripción: "Galletas artesanales de perro ", Valor_unitario: "$6.000", Estado: "Disponible" },
+  { ID: 6, descripción: "Cama de perro", Valor_unitario: "$35.000", Estado: "Disponible" },
 ];
-
+const stateToAvailable = { Disponible: true, "No Disponible": false }
 class App extends React.Component {
   state = {
     data: data,
@@ -31,7 +31,7 @@ class App extends React.Component {
       ID: "",
       Descripción: "",
       Valor_unitario: "",
-      Estado:"",
+      Estado: "",
     },
   };
 
@@ -56,12 +56,23 @@ class App extends React.Component {
     this.setState({ modalInsertar: false });
   };
 
+  listar = async () => {
+    try {
+      const resp = await fetch("http://localhost:3002/api/products", { method: "GET" })
+      const data = await resp.json()
+      this.setState({ data: data })
+      console.log(data)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   editar = (dato) => {
     var contador = 0;
     var arreglo = this.state.data;
     arreglo.map((registro) => {
       if (dato.ID == registro.ID) {
-         arreglo[contador].ID = dato.ID;
+        arreglo[contador].ID = dato.ID;
         arreglo[contador].descripción = dato.descripción;
         arreglo[contador].Valor_unitario = dato.Valor_unitario;
         arreglo[contador].Estado = dato.Estado;
@@ -72,7 +83,7 @@ class App extends React.Component {
   };
 
   eliminar = (dato) => {
-    var opcion = window.confirm("Estás Seguro que deseas Eliminar el elemento "+dato.ID);
+    var opcion = window.confirm("Estás Seguro que deseas Eliminar el elemento " + dato.ID);
     if (opcion == true) {
       var contador = 0;
       var arreglo = this.state.data;
@@ -86,17 +97,29 @@ class App extends React.Component {
     }
   };
 
-  insertar= ()=>{
-    var valorNuevo= {...this.state.form};
-    
-    var lista= this.state.data;
-    lista.push(valorNuevo);
+  insertar = async () => {
+    try {
+      const resp = await fetch("http://localhost:3002/api/products", {
+        method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify({
+          ...this.state.form,
+          disponible: stateToAvailable[this.state.form.disponible]
+
+        })
+      })
+      const data = await resp.text()
+      console.log(data)
+    } catch (err) {
+      console.log(err)
+    }
+
+    const lista = this.state.data;
+    lista.push(this.state.form);
     this.setState({ modalInsertar: false, data: lista });
   }
 
-  onChange=async e=>{
+  onChange = async e => {
     e.persist();
-    await this.setState({busqueda:e.target.value});
+    await this.setState({ busqueda: e.target.value });
     /*console.log(this.state.busqueda);
     console.log('lista filtrada',this.state.data.filter((elemento)=>{
       console.log("elemento",elemento);
@@ -104,21 +127,21 @@ class App extends React.Component {
     })
     );
     esto era para vefiricar salida por consola*/
-    
+
     this.filtrar();  /*Se llama a la funcion filtar cada que se escribe en el buscador*/
   }
-  filtrar=()=>{
-    var search=data.filter((elemento)=>{
-      if(JSON.stringify(elemento).toLowerCase().includes(this.state.busqueda.toLowerCase())){
+  filtrar = () => {
+    var search = data.filter((elemento) => {
+      if (JSON.stringify(elemento).toLowerCase().includes(this.state.busqueda.toLowerCase())) {
         return elemento;
       }
     });
-    this.setState({data:search}); /*actualiza la tabla*/
+    this.setState({ data: search }); /*actualiza la tabla*/
   }
 
-  componentDidMount(){
-    this.setState({data:data});
-  } 
+  componentDidMount() {
+    this.listar()
+  }
 
   handleChange = (e) => {
     this.setState({
@@ -130,15 +153,15 @@ class App extends React.Component {
   };
 
   render() {
-    
+
     return (
       <>
         <Container >
-        <br />
-        
-          <Button color="success"  onClick={()=>this.mostrarModalInsertar()}>Crear un nuevo producto</Button>
-          <input name="busqueda" value={this.state.busqueda} placeholder='buscar' className='border-gray-700 px-2 py-1  '  onChange={this.onChange}/>
-         
+          <br />
+
+          <Button color="success" onClick={() => this.mostrarModalInsertar()}>Crear un nuevo producto</Button>
+          <input name="busqueda" value={this.state.busqueda} placeholder='buscar' className='border-gray-700 px-2 py-1  ' onChange={this.onChange} />
+
           <br />
           <br />
           <br />
@@ -146,20 +169,23 @@ class App extends React.Component {
             <thead>
               <tr>
                 <th>ID</th>
-                <th>Descripción</th>
-                <th>Valor Unitario</th>
-                <th>Estado</th>
-                <th>Acción</th>
+                <th>Tille</th>
+                <th>Price</th>
+                <th>URL</th>
+                <th>Categoría</th>
+                <th>Disponible</th>
               </tr>
             </thead>
 
             <tbody>
               {this.state.data.map((dato) => (
-                <tr key={dato.ID}>
-                  <td>{dato.ID}</td>
-                  <td>{dato.descripción}</td>
-                  <td>{dato.Valor_unitario}</td>
-                  <td>{dato.Estado}</td>
+                <tr key={dato._id}>
+                  <td>{dato._id}</td>
+                  <td>{dato.title}</td>
+                  <td>{dato.price}</td>
+                  <td>{dato.url}</td>
+                  <td>{dato.categoria}</td>
+                  <td>{dato.disponible ? "Disponible" : "No disponible"}</td>
                   <td>
                     <Button
                       color="primary"
@@ -167,7 +193,7 @@ class App extends React.Component {
                     >
                       Editar
                     </Button>{" "}
-                    <Button color="secondary" onClick={()=> this.eliminar(dato)}>Eliminar</Button>
+                    <Button color="secondary" onClick={() => this.eliminar(dato._id)}>Eliminar</Button>
                   </td>
                 </tr>
               ))}
@@ -177,63 +203,91 @@ class App extends React.Component {
 
         <Modal isOpen={this.state.modalActualizar}>
           <ModalHeader>
-           <div><h3>Editar Registro</h3></div>
+            <div><h3>Editar Registro</h3></div>
           </ModalHeader>
 
           <ModalBody>
             <FormGroup>
               <label>
-               ID:
+                Nombre
               </label>
-            
+
               <input
                 className="form-control"
                 readOnly
-                name="ID"
+                name="title"
                 type="text"
                 onChange={this.handleChange}
-                value={this.state.form.ID}
-              />
-            </FormGroup>
-            
-            <FormGroup>
-              <label>
-                Descripción: 
-              </label>
-              <input
-                className="form-control"
-                name="descripción"
-                type="text"
-                onChange={this.handleChange}
-                value={this.state.form.descripción}
-              />
-            </FormGroup>
-            
-            <FormGroup>
-              <label>
-                Valor unitario: 
-              </label>
-              <input
-                className="form-control"
-                name="Valor_unitario"
-                type="text"
-                onChange={this.handleChange}
-                value={this.state.form.Valor_unitario}
+                value={this.state.form.title}
               />
             </FormGroup>
 
             <FormGroup>
               <label>
-                Estado: 
+                Precio
               </label>
-              <select  className="form-control" name="Estado" value={this.state.form.Estado} onChange={this.handleChange}>
-               <option selected value="0">Elige una opcion</option>
-               <option>Disponible</option> 
-                <option>No Disponible</option> 
-                
+
+              <input
+                className="form-control"
+                readOnly
+                name="price"
+                type="number"
+                onChange={this.handleChange}
+                value={this.state.form.price}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label>
+                Descripción:
+              </label>
+              <input
+                className="form-control"
+                name="description"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.description}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label>
+                URL
+              </label>
+              <input
+                className="form-control"
+                name="url"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.url}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label>
+                Categoría
+              </label>
+              <input
+                className="form-control"
+                name="categoria"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.categoria}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label>
+                Estado:
+              </label>
+              <select className="form-control" name="disponible" value={this.state.form.disponible} onChange={this.handleChange}>
+                <option selected value="0">Elige una opcion</option>
+                <option>Disponible</option>
+                <option>No Disponible</option>
+
               </select>
             </FormGroup>
-            
+
           </ModalBody>
 
           <ModalFooter>
@@ -256,56 +310,87 @@ class App extends React.Component {
 
         <Modal isOpen={this.state.modalInsertar}>
           <ModalHeader>
-           <div><h3>Registrar producto </h3></div>
+            <div><h3>Registrar producto </h3></div>
           </ModalHeader>
 
           <ModalBody>
+
             <FormGroup>
               <label>
-                ID: 
+                Nombre
               </label>
-              
+
               <input
                 className="form-control"
-                name="ID"
+                name="title"
                 type="text"
                 onChange={this.handleChange}
-              />
-            </FormGroup>
-            
-            <FormGroup>
-              <label>
-                descripción: 
-              </label>
-              <input
-                className="form-control"
-                name="descripción"
-                type="text"
-                onChange={this.handleChange}
-              />
-            </FormGroup>
-            
-            <FormGroup>
-              <label>
-                Valor_unitario: 
-              </label>
-              <input
-                className="form-control"
-                name="Valor_unitario"
-                type="text"
-                onChange={this.handleChange}
+                value={this.state.form.title}
               />
             </FormGroup>
 
             <FormGroup>
               <label>
-                Estado: 
+                Precio
               </label>
-              <select  className="form-control" name="Estado" value={this.state.form.Estado} onChange={this.handleChange}>
-               <option selected value="0">Elige una opcion</option>
-               <option>Disponible</option> 
-                <option>No Disponible</option> 
-                
+
+              <input
+                className="form-control"
+                name="price"
+                type="number"
+                onChange={this.handleChange}
+                value={this.state.form.price}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label>
+                Descripción:
+              </label>
+              <input
+                className="form-control"
+                name="description"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.description}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label>
+                URL
+              </label>
+              <input
+                className="form-control"
+                name="url"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.url}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label>
+                Categoría
+              </label>
+              <input
+                className="form-control"
+                name="categoria"
+                type="text"
+                onChange={this.handleChange}
+                value={this.state.form.categoria}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <label>
+                Estado:
+              </label>
+              <select className="form-control" name="disponible" value={this.state.form.disponible} onChange={this.handleChange}>
+                <option selected value="0">Elige una opcion</option>
+                <option>Disponible</option>
+                <option>No Disponible</option>
+
               </select>
             </FormGroup>
 
